17fa2621f000f9504bca94d3881ab2a221a6cea4
nirajdobariya <nirajdobariya.albetrios@gmail.com>
2022-03-17 17:45:12 +0530

    second commit

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -42,22 +42,6 @@ const reducer = (state = initialState, action) => {
       };
   }
 
-  // if(action.type === 'INCREMENT'){
-  //     return {
-  //         counter: state.counter + 1
-  //     }
-  // }
-  // if(action.type === 'DECREMENT'){
-  //     return {
-  //         counter: state.counter - 1
-  //     }
-  // }
-  // if(action.type === 'ADD'){
-  //     return {
-  //         counter: state.counter + action.val
-  //     }
-  // }
-
   return state;
 };
 
